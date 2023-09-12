67eff31382844699bd224c7e2269c81c1e694cb9
anhilmy <hilmyahmadnaufal@gmail.com>
2023-09-12 14:14:12 +0700

    feat: add readme

--- src/app/kegiatan/[id]/page.tsx
+++ src/app/kegiatan/[id]/page.tsx
@@ -1,4 +1,3 @@
-import Modal from '@/components/Modal'
 import Navbar from '@/components/Navbar'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -9,9 +8,7 @@ import { useRouter } from 'next/navigation'
 export default function DetailSuratTugas({ params }: { params: { id: string } }) {
   return (
     <>
-      <Modal>
-        <p>Surat Tugas yang dicari adalah: {params.id}</p>
-      </Modal>
+
     </>
   )
 }
